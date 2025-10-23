dbc979612b32099ab890a60035c993e85a47a5ca
Jan Konieczek <koleks92@gmail.com>
2025-10-23 21:59:20 +0200

    feat: Electron + start point

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("Avocadoro from Electron 🥑")
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+    });
+
+    win.loadFile("react/index.html");
+};
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});
