a01361ae419a91811dd8dd9461c71a70da9027d3
Jan Konieczek <koleks92@gmail.com>
2025-10-23 22:11:23 +0200

    feat: Added react app, electron-builder pacakges

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
     });
 
-    win.loadFile("react/index.html");
+    const startURL = isDev
+        ? "http://localhost:3000"
+        : `file://${path.join(__dirname, "../build/index.html")}`;
+
+    win.loadFile(startURL);
 };
 
 app.whenReady().then(() => {
     createWindow();
-
-    app.on("activate", () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
 });
 
 app.on("window-all-closed", () => {
