d6f738414ca2f8f517ac2c138f53ffe92fdc1f42
Jan Konieczek <koleks92@gmail.com>
2025-10-24 13:26:11 +0200

    feat: Supabase connection working

--- src/react/app.tsx
+++ src/react/app.tsx
@@ -1,5 +1,44 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
 
+// Get .env and create supabase clinet
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+// Types
+type instrument = {
+    id: number;
+    name: string;
+};
+
+function App() {
+    const [instruments, setInstruments] = useState<instrument[]>([]);
+
+    useEffect(() => {
+        getInstruments();
+    }, []);
+
+    async function getInstruments(): Promise<void> {
+        const { data, error } = await supabase.from("instruments").select();
+        if (error) {
+            console.error("Error: ", error);
+        }
+        setInstruments(data);
+        console.log(data);
+    }
+
+    return (
+        <>
+            <div>sss</div>
+            <ul>
+                {instruments.map((instrument) => (
+                    <li key={instrument.name}>{instrument.name}</li>
+                ))}
+            </ul>
+        </>
+    );
+}
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(<App />);
---
+++ src/vite-env.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="vite/client" />
+declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string | undefined;
+declare const MAIN_WINDOW_VITE_NAME: string;
