85118f09391a6fc09e770ed1454b01d2ef76451e
Jan Konieczek <koleks92@gmail.com>
2025-11-05 13:48:17 +0100

    feat: List of session groups, links

--- src/react/dashboard.tsx
+++ src/react/dashboard.tsx
@@ -1,23 +1,83 @@
 import "../index.css";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { AvocadoroContext } from "./store/AvocadoroContext";
+import { ThreeDot } from "react-loading-indicators";
+
+type SessionGroups = {
+    id: string;
+    name: string;
+};
 
 export default function Dashboard() {
+    const [loading, setLoading] = useState<boolean>(true);
+    const [sessionGroups, setSessionGroups] = useState<SessionGroups[]>([]);
+
     const { session, supabase } = useContext(AvocadoroContext);
     const navigate = useNavigate();
 
-    async function signOut() {
+    useEffect(() => {
+        const loadGroups = async () => {
+            if (!session || !session.user) return; // Wait until session is ready
+
+            setLoading(true);
+
+            const { data, error } = await supabase
+                .from("session_groups")
+                .select("*")
+                .eq("user_id", session.user.id);
+
+            if (error) {
+                console.error("Error loading groups:", error);
+            } else {
+                setSessionGroups(data);
+            }
+
+            setTimeout(() => {
+                setLoading(false);
+            }, 500);
+        };
+
+        loadGroups();
+    }, [session]); // 👈 This reruns whenever session changes
+
+    async function signOut(): Promise<void> {
         const { error } = await supabase.auth.signOut();
 
         navigate("/");
     }
 
+    if (loading) {
+        return (
+            <div className="test">
+                <ThreeDot
+                    color="#32cd32"
+                    size="medium"
+                    text=""
+                    textColor=""
+                />{" "}
+            </div>
+        );
+    }
+
     return (
         <div className="vertical_test">
-            Dashboard
-            <button onClick={() => {navigate("/add_group")}}>Add new group</button>
             <button onClick={signOut}>Log out</button>
+            Dashboard
+            <button
+                onClick={() => {
+                    navigate("/add_group");
+                }}
+            >
+                Add new group
+            </button>
+            <div className="vertical_test">
+                {sessionGroups.map((group) => 
+                    <div onClick={() => navigate(`/group/${group.id}`)}>{group.name}</div>
+                    
+
+                )}
+            </div>
         </div>
     );
 }
