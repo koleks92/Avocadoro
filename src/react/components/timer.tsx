7c5375a3257bcf4dabd398f150a7f9004f277b38
Jan Konieczek <koleks92@gmail.com>
2025-11-05 21:44:05 +0100

    feat: Timer working, only fixed timers from session groups

---
+++ src/react/components/timer.tsx
@@ -0,0 +1,93 @@
+import "../../index.css";
+
+import { useEffect, useRef, useState } from "react";
+import Button from "./button";
+
+type timerModeType = "focus" | "break";
+
+type TimerProps = {
+    onComplete?: (minutes: number) => void;
+    focus_timer?: number;
+    break_timer?: number
+}
+
+function Timer({onComplete, focus_timer, break_timer}: TimerProps) {
+    const [focusTimer, setFocusTimer] = useState<number>(focus_timer);
+    const [breakTimer, setBreakTimer] = useState<number>(break_timer);
+
+    const [timerMode, setTimerMode] = useState<timerModeType>("focus");
+
+    const [minutes, setMinutes] = useState<number>(focusTimer);
+    const [seconds, setSeconds] = useState<number>(0);
+
+    const [message, setMessage] = useState<string>("");
+
+    const timerRef = useRef<number | null>(null);
+
+    useEffect(() => {
+
+    })
+
+    useEffect(() => {
+        if (timerRef.current === null) return;
+
+        if (seconds <= 0) {
+            setSeconds(59);
+            setMinutes((prev) => prev - 1);
+        }
+    }, [seconds]);
+
+    useEffect(() => {
+        if (seconds === 0 && minutes === 0) {
+            setSeconds(0);
+            if (timerMode === "break") {
+                // Set focus mode
+                setMessage("Focus Time");
+                setMinutes(focusTimer);
+                setTimerMode("focus");
+            } else {
+                // Pass the complete session
+                onComplete(focusTimer);
+                // Set break mode
+                setMessage("Break Time");
+                setMinutes(breakTimer);
+                setTimerMode("break");
+            }
+        }
+    }, [seconds, minutes]);
+
+    const start = (): void => {
+        if (timerRef.current !== null) return; // prevent multiple intervals
+        timerRef.current = window.setInterval(() => {
+            setSeconds((prev) => prev - 1);
+        }, 1000);
+    };
+
+    const stop = (): void => {
+        if (timerRef.current !== null) {
+            clearInterval(timerRef.current);
+            timerRef.current = null;
+        }
+    };
+
+    const reset = (): void => {
+        if (timerRef.current !== null) {
+            clearInterval(timerRef.current);
+        }
+        timerRef.current = null;
+        setMinutes(focusTimer);
+        setSeconds(0);
+    };
+    return (
+        <div className="vertical_test">
+            <span>Timer</span>
+            {minutes}:{seconds}
+            <span>{message}</span>
+            <Button type="button" label="Start" onClick={() => start()} />
+            <Button type="button" label="Stop" onClick={() => stop()} />
+            <Button type="button" label="Reset" onClick={() => reset()} />
+        </div>
+    );
+}
+
+export default Timer;
