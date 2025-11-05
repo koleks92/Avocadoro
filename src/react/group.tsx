7b91b294b7e777a778a7e8f430fa13299a4a30a4
Jan Konieczek <koleks92@gmail.com>
2025-11-05 21:43:38 +0100

    feat: onCompleteHandler, Timer component visible

--- src/react/group.tsx
+++ src/react/group.tsx
@@ -1,7 +1,38 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
+import Timer from "./components/timer";
+import { useContext, useState } from "react";
+import { AvocadoroContext } from "./store/AvocadoroContext";
 
 export default function Group() {
     const { id } = useParams<{ id: string }>();
+    const { state } = useLocation();
 
-    return <div>Group {id}</div>;
+    const [message, setMessage] = useState<string>("");
+
+    const { session, supabase } = useContext(AvocadoroContext);
+
+    async function onCompleteHandler(minutes: number): Promise<void> {
+        setMessage("");
+
+        // Insert data
+        const { data, error } = await supabase
+            .from("sessions")
+            .insert({
+                session_group_id: id,
+                duration_minutes: minutes,
+            })
+            .select();
+
+        if (error) {
+            setMessage(error.message);
+        }
+    }
+
+    return (
+        <div>
+            <span>Group {id}</span>
+            <Timer onComplete={onCompleteHandler} focus_timer={state.focus_timer} break_timer={state.break_timer} />
+            {message}
+        </div>
+    );
 }
