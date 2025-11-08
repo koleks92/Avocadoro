f7172ee9256470eb3c610adcc77a310d98ea68cd
Jan Konieczek <koleks92@gmail.com>
2025-11-08 22:21:53 +0100

    feat: Created loading component

---
+++ src/react/components/loading.tsx
@@ -0,0 +1,9 @@
+import { ThreeDot } from "react-loading-indicators";
+
+export default function Loading() {
+    return (
+        <div>
+            <ThreeDot color="#32cd32" size="medium" text="" textColor="" />
+        </div>
+    );
+}
