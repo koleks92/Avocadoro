0f15418eec0540d3d2d1fbcad78cf9fcd2e19b0a
Jan Konieczek <koleks92@gmail.com>
2025-11-09 12:52:43 +0100

    feat: Added style prop

--- src/react/components/button.tsx
+++ src/react/components/button.tsx
@@ -4,17 +4,18 @@ type Props = {
     label: string;
     onClick?: () => void;
     type: "button" | "submit" | "reset";
+    style?: "standard" | "noBackground";
     disabled?: boolean;
 };
 
-function Button({ label, onClick, type, disabled }: Props) {
+function Button({ label, onClick, type, style, disabled }: Props) {
     return (
         <div>
             <button
                 onClick={onClick}
                 type={type}
                 disabled={disabled}
-                className="custom_button"
+                className={`custom_button ${style} ${style === "noBackground" && "button_nobg"}`}
             >
                 {label}
             </button>
