eab5ab128297975f5d63da583b2dd609c03bfd23
Jan Konieczek <koleks92@gmail.com>
2025-11-17 11:13:40 +0100

    feat: Styling, converting type func, scroll animation

--- src/react/group.tsx
+++ src/react/group.tsx
@@ -1,17 +1,60 @@
 import { useParams, useLocation, useNavigate } from "react-router-dom";
 import Timer from "./components/timer";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AvocadoroContext } from "./store/AvocadoroContext";
 import Button from "./components/button";
+import { IoIosArrowBack, IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { IoPencil } from "react-icons/io5";
+import AvocadoroPrint from "./components/avocadoroPrint";
 
 export default function Group() {
     const { id } = useParams<{ id: string }>();
     const { state } = useLocation();
     const navigate = useNavigate();
 
+    const [timerView, setTimerView] = useState<boolean>(false);
+
     const [message, setMessage] = useState<string>("");
 
+    const [avocadoroAmount, setAvocadoroAmount] = useState<number>(0);
+    const [totalTime, setTotalTime] = useState<string>("");
+
     const { session, supabase } = useContext(AvocadoroContext);
+
+    useEffect(() => {
+        setAvocadoroAmount(Math.floor(state.total_minutes / 30));
+
+        function convertTime(): void {
+            const hours = Math.floor(state.total_minutes / 60);
+            const minutes = state.total_minutes % 60;
+
+            // Pad with leading zeros if needed
+            const paddedHours = String(hours).padStart(2, "0");
+            const paddedMinutes = String(minutes).padStart(2, "0");
+
+            setTotalTime(`${paddedHours}h ${paddedMinutes}m`);
+        }
+
+        convertTime();
+    }, []);
+
+    useEffect(() => {
+        let timeout: ReturnType<typeof setTimeout>;
+
+        const el = document.querySelector(
+            ".group_second_div"
+        ) as HTMLElement | null;
+
+        if (timerView === false && el) {
+            timeout = setTimeout(() => {
+                el.classList.add("delay-scroll");
+            }, 500);
+        } else if (el) {
+            el.classList.remove("delay-scroll");
+        }
+
+        return () => clearTimeout(timeout);
+    }, [timerView]);
 
     async function onCompleteHandler(minutes: number): Promise<void> {
         setMessage("");
@@ -31,28 +74,79 @@ export default function Group() {
     }
 
     return (
-        <div>
-            <span>Group {id}</span>
-            <Button
-                label="Go Back"
-                type="button"
-                onClick={() => {
-                    navigate("/dashboard");
-                }}
-            />
-            <Button
-                label="Edit"
-                type="button"
-                onClick={() => {
-                    navigate(`/edit_group/${state.id}`, { state: {...state, edit: true} });
-                }}
-            />
-            <Timer
-                onComplete={onCompleteHandler}
-                focus_timer={state.focus_timer}
-                break_timer={state.break_timer}
-            />
-            {message}
+        <div className="group_root">
+            <div className="group_logo_div">
+                <div>
+                    <Button
+                        onClick={() => navigate("/dashboard")}
+                        type="button"
+                        style="custom_button button_logo_dashboard"
+                        label={<IoIosArrowBack />}
+                    />
+                </div>
+                <span className="dashboard_title_span">{state.name}</span>
+                <div>
+                    <Button
+                        onClick={() => {
+                            navigate(`/edit_group/${state.id}`, {
+                                state: { ...state, edit: true },
+                            });
+                        }}
+                        type="button"
+                        style="custom_button button_logo_group"
+                        label={<IoPencil />}
+                    />
+                </div>
+            </div>
+            <div className="group_div_main">
+                {/* Results view */}
+                <div
+                    className={`group_result_div ${timerView ? "group_hidden_view" : "group_shown_view"}`}
+                >
+                    <div className="group_second_div">
+                        <span className="group_second_div_title">
+                            Total focus time
+                        </span>
+                        <span className="group_second_div_time">
+                            {" "}
+                            {totalTime}
+                        </span>
+                        <AvocadoroPrint amount={avocadoroAmount} />
+                        {message}
+                    </div>
+
+                    <div className="group_button_div">
+                        <Button
+                            onClick={() => setTimerView(true)}
+                            type="button"
+                            style="custom_button button_logo_group"
+                            label={<IoIosArrowUp />}
+                        />
+                    </div>
+                </div>
+                {/* Timer view */}
+                <div
+                    className={`group_timer_div ${timerView ? "group_shown_view" : "group_hidden_view"}`}
+                >
+                    <div className="group_button_div">
+                        <Button
+                            onClick={() => setTimerView(false)}
+                            type="button"
+                            style="custom_button button_logo_group"
+                            label={<IoIosArrowDown />}
+                        />
+                    </div>
+
+                    <div className="group_second_div">
+                        <Timer
+                            onComplete={onCompleteHandler}
+                            focus_timer={state.focus_timer}
+                            break_timer={state.break_timer}
+                        />
+                        {message}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
