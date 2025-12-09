9135f93831dca33c25712f909a2da8abfe967f02
Jan Konieczek <koleks92@gmail.com>
2025-12-09 11:33:29 +0100

    feat/fix: Switched to HashRouter due to build fails

--- src/react/app.tsx
+++ src/react/app.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { AvocadoroProvider } from "./store/AvocadoroContext";
 import AnimatedRoutes from "./components/animatedRoutes";
 
 export default function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <AvocadoroProvider>
                 <AnimatedRoutes />
             </AvocadoroProvider>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
