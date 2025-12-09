7bcda5d67aa96c4bf3cee4d737d7e8c0cce926eb
Jan Konieczek <koleks92@gmail.com>
2025-12-09 22:10:55 +0100

    feat: Added onDeepLinkUrl context

--- src/preload.ts
+++ src/preload.ts
@@ -1,14 +1,23 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-import { contextBridge, ipcRenderer } from 'electron'
+import { contextBridge, ipcRenderer } from "electron";
 
 declare global {
-  interface Window {
-    electronAPI: any;
-  }
+    interface Window {
+        electronAPI: any;
+    }
 }
 
-contextBridge.exposeInMainWorld('electronAPI', {
-  setTimer: (timer: string) => ipcRenderer.send('setTimer', timer),
-  setTimerOn: (timerOn: boolean) => ipcRenderer.send('setTimerOn', timerOn)
-})
+contextBridge.exposeInMainWorld("electronAPI", {
+    setTimer: (timer: string) => ipcRenderer.send("setTimer", timer),
+    setTimerOn: (timerOn: boolean) => ipcRenderer.send("setTimerOn", timerOn),
+    onDeepLinkUrl: (callback: (url: string) => void) => {
+        const listener = (_event: Electron.IpcRendererEvent, url: string) =>
+            callback(url);
+        ipcRenderer.on("deep-link-url", listener);
+
+        return () => {
+            ipcRenderer.removeListener("deep-link-url", listener);
+        };
+    },
+});
