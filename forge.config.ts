8fc698bc3e806c2b3336ce61c9c09f159ffceddb
Jan Konieczek <koleks92@gmail.com>
2025-12-10 11:23:00 +0100

    feat: Icons added for Windows, Linux, Macos

--- forge.config.ts
+++ forge.config.ts
@@ -10,7 +10,8 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    extraResource: ["./assets"]
+    extraResource: ["./assets"],
+    icon: './assets/icon'
   },
   rebuildConfig: {},
   makers: [
