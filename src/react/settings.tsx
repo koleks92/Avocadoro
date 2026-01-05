5ac9f30b58c2afc3b2562b9a905725566aa69770
Jan Konieczek <koleks92@gmail.com>
2026-01-05 21:34:41 +0100

    feat: Added top div

--- src/react/settings.tsx
+++ src/react/settings.tsx
@@ -1,11 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import Button from "./components/button";
 import MotionDiv from "./components/motionDiv";
+import { IoIosArrowBack } from "react-icons/io";
 
 export default function Settings() {
+    const navigate = useNavigate();
+
     return (
         <MotionDiv>
             <div className="settings_root">
-                Test
+                <div className="group_logo_div">
+                    <div>
+                        <Button
+                            onClick={() => navigate(-1)}
+                            type="button"
+                            style="custom_button button_logo_dashboard"
+                            label={<IoIosArrowBack />}
+                        />
+                    </div>
+                    <span className="dashboard_title_span">Settings</span>
+                    <div style={{ visibility: "hidden" }}>
+                        <Button
+                            onClick={() => navigate(-1)}
+                            type="button"
+                            style="custom_button button_logo_dashboard"
+                            label={<IoIosArrowBack />}
+                        />
+                    </div>
+                </div>
             </div>
         </MotionDiv>
-    )
+    );
 }
