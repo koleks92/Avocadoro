cd1b283ecaf9079d6a26d224f2ae8ee22c534a0a
Jan Konieczek <koleks92@gmail.com>
2026-01-07 22:34:48 +0100

    test: Added test for deleting user account

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -6,12 +6,18 @@ describe("Authentication Flow", () => {
     const passwordInput = () => $('input[placeholder="Type password"]');
     const confirmPasswordInput = () =>
         $('input[placeholder="Confirm password"]');
+
     const loginButton = () => $("button=Log in");
     const signUpSubmitButton = () => $("button=Sign Up");
     const goToSignUpButton = () => $("button*=have an account yet");
     const backButton = () =>
         $(".login_logo_div > div:first-child .button_logo");
     const logoutButton = () => $(".button_logo_dashboard");
+    const settingsButton = () => $(".dashboard_bottom_div .custom_button");
+    const removeAccountButton = () => $(".settings_main_div .custom_button");
+    const removeAccountButton2 = () =>
+        $(".settings_confirm_div .button_logo_dashboard");
+
     const dashboardTitle = () => $(".dashboard_title_span");
 
     const passwordWarning = () =>
@@ -38,7 +44,7 @@ describe("Authentication Flow", () => {
         });
     });
 
-    describe("Create new account", () => {
+    describe("Create new account, delete account", () => {
         it("Create new account, move to dashboard and logout", async () => {
             await expect(loginButton()).toBeDisplayed();
             await goToSignUpButton().click();
@@ -69,6 +75,18 @@ describe("Authentication Flow", () => {
             await confirmPasswordInput().setValue("Password12345");
             await signUpSubmitButton().click();
             await expect(passwordsWarning()).toBeDisplayed();
+        });
+        it("Delete created account", async () => {
+            await (browser as any).refresh();
+            await expect(loginButton()).toBeDisplayed();
+            await emailInput().setValue("test2@mail.com");
+            await passwordInput().setValue("Password123456");
+            await loginButton().click();
+            await expect(dashboardTitle()).toBeDisplayed();
+            await settingsButton().click();
+            await removeAccountButton().click();
+            await removeAccountButton2().click();
+            await expect(loginButton()).toBeDisplayed();
         });
     });
 
